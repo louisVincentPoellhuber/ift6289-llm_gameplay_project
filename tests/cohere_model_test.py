1a0e81d9abf2438c6e60a9ffce42b4bf2dc4e115
Rafaela Pinter <rpinter811@gmail.com>
2024-03-10 18:36:13 -0400

    testing cohere

--- tests/cohere_model_test.py
+++ tests/cohere_model_test.py
@@ -4,17 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-co = cohere.Client(os.getenv("COHERE_API_KEY"))
+co = cohere.Client(os.getenv("COHEREAI_API_KEY"))
 
 response = co.chat(
-  chat_history=[
-    {"role": "USER", "message": "Who discovered gravity?"},
-    {"role": "CHATBOT", "message": "The man who is widely credited with \
-     discovering gravity is Sir Isaac Newton"}
-  ],
-  message="What year was he born?",
-  # perform web search before answering the question. You can also use your own custom connector.
-#   connectors=[{"id": "web-search"}]
+    chat_history=[
+        {"role": "USER", "message": "Who discovered gravity?"},
+        {
+            "role": "CHATBOT",
+            "message": "The man who is widely credited with \
+     discovering gravity is Sir Isaac Newton",
+        },
+    ],
+    message="What year was he born?",
+    # perform web search before answering the question. You can also use your own custom connector.
+    #   connectors=[{"id": "web-search"}]
 )
 
 print(response.text)

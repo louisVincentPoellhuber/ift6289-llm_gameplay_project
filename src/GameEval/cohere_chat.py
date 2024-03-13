0c59b23b28fd371b48610fd6122588bb6793176b
Rafaela Pinter <rpinter811@gmail.com>
2024-03-13 15:21:49 -0400

    merge updates

--- src/GameEval/cohere_chat.py
+++ src/GameEval/cohere_chat.py
@@ -1,35 +1,56 @@
+import cohere
 import sys
 import openai
 from func_timeout import func_set_timeout
 
-from chat.config import key_gpt3, api_type_gpt3, api_base_gpt3, api_version_gpt3, engine_gpt3, temperature_gpt3
+# from chat.config import (
+#     key_gpt3,
+#     api_type_gpt3,
+#     api_base_gpt3,
+#     api_version_gpt3,
+#     engine_gpt3,
+#     temperature_gpt3,
+# )
+
+import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+CHATARENA_PATH = os.getenv("CHATARENA_PATH")
+sys.path.append(CHATARENA_PATH)
+
 
 @func_set_timeout(15)
 def get_response(messages):
     response = co.chat(
-    chat_history=[
-        {"role": "USER", "message": "Who discovered gravity?"},
-        {"role": "CHATBOT", "message": "The man who is widely credited with \
-        discovering gravity is Sir Isaac Newton"}
-    ],
-    message="What year was he born?",
-    # perform web search before answering the question. You can also use your own custom connector.
-    connectors=[{"id": "web-search"}]
+        chat_history=[
+            {"role": "USER", "message": "Who discovered gravity?"},
+            {
+                "role": "CHATBOT",
+                "message": "The man who is widely credited with \
+        discovering gravity is Sir Isaac Newton",
+            },
+        ],
+        message="What year was he born?",
+        # perform web search before answering the question. You can also use your own custom connector.
+        connectors=[{"id": "web-search"}],
     )
     return response
+
 
 class COHERE:
     def __init__(self) -> None:
         self.name = "cohere"
         self.co = cohere.Client(os.getenv("COHERE_API_KEY"))
 
-    def single_chat(self,content,role=None):
+    def single_chat(self, content, role=None):
         if role is None:
             role = "You are an AI assistant that helps people find information."
         chat_history = [
-                    {"role":"USER","message":role},
-                    {"role":"USER","message":content}
-                    ]
+            {"role": "USER", "message": role},
+            {"role": "USER", "message": content},
+        ]
         res = None
         cnt = 0
         while True:
@@ -38,11 +59,11 @@ class COHERE:
                 res = response["choices"][0]["message"]["content"]
                 break
             except:
-                cnt += 1 
+                cnt += 1
             if cnt >= 5:
-                break    
-       
-        return  res
+                break
+
+        return res
 
     def multi_chat(self, messages):
 
@@ -55,11 +76,12 @@ class COHERE:
                 res = response["choices"][0]["message"]["content"]
                 break
             except:
-                cnt += 1 
+                cnt += 1
             if cnt >= 3:
-                break    
+                break
 
-        return  res
-    
+        return res
+
+
 if __name__ == "__main__":
     pass

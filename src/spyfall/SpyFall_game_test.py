4fdbb156fa9256c597d130dfa6a5f1de10d08382
Rafaela Pinter <rpinter811@gmail.com>
2024-04-11 18:26:00 -0400

    changing folder

--- src/spyfall/SpyFall_game_test.py
+++ src/spyfall/SpyFall_game_test.py
@@ -38,8 +38,8 @@ with open(PROMPT_CONFIG_FILE) as file:
 
 # prompt_mode = "remember_json"
 prompt_modes = [
-    "baseline",
-    # "remember_json_guessing_format",
+    # "baseline",
+    "remember_json_guessing_format",
     # "remember_sentence_format",
     # "remember_bracket_format"
 ]
@@ -66,7 +66,7 @@ for prompt_mode in prompt_modes:
         players_list = [
             Player(
                 name=players[i],
-                role_desc=role_description + format_specification,
+                role_desc=role_description,  # + format_specification,
                 backend=backend,
             )
             for i in range(len(players[:number_of_players]))
@@ -84,5 +84,5 @@ for prompt_mode in prompt_modes:
         postfix = f"{prompt_mode}_{strftime('%Y_%m_%d_%H_%M_%S')}"
         # Saving history
         arena.save_chat(
-            f"src/spyfall/chat_history/29_03_24/spyfall_{prompt_mode}_{strftime('%Y_%m_%d_%H_%M_%S')}.json"
+            f"src/spyfall/chat_history/11_04_24/spyfall_{prompt_mode}_{strftime('%Y_%m_%d_%H_%M_%S')}.json"
         )
